146975935f9c25cae60d11db695b0009a7f545ef
preethi <pattlollapreethi@gmail.com>
2025-12-25 22:07:28 +0530

    Remove static file serving for production
    Removed production environment static file serving.

--- backend/server.js
+++ backend/server.js
@@ -37,12 +37,7 @@ app.use("/api/cart", cartRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/payment", paymentsRoutes);
 app.use("/api/customer-service", customerServiceRoutes);
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "frontend/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  });
-}
+
 
 
 // Start server
