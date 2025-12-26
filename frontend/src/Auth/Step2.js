e96edeed3e60864520c33d4eae1305fab84308ad
preethi <pattlollapreethi@gmail.com>
2025-12-26 12:25:39 +0530

    Refactor Step2 component to use Link instead of anchor

--- frontend/src/Auth/Step2.js
+++ frontend/src/Auth/Step2.js
@@ -1,7 +1,7 @@
 // src/Auth/Step2.js
 import React, { useEffect, useState } from "react";
 import amazon from "../HomePage/images/Amazon_black.png";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
 const Step2 = () => {
   const [identifier, setIdentifier] = useState("");
@@ -22,10 +22,12 @@ const Step2 = () => {
   return (
     <div className="flex flex-col items-center mt-20">
       <img src={amazon} alt="Amazon" className="w-20 mb-5" />
-      <div className="border border-gray-400 p-6 w-[350px]">
+
+      <div className="border border-gray-400 p-6 w-[350px] rounded bg-white">
         <h2 className="text-xl font-bold mb-2">
           {name ? `Welcome ${name}, ` : ""}Looks like you are new to Amazon
         </h2>
+
         <p className="mb-2">
           {identifier}{" "}
           <span
@@ -35,32 +37,40 @@ const Step2 = () => {
             change
           </span>
         </p>
+
         <p>Let's create an account using your mobile number</p>
+
         <button
           onClick={proceed}
-          className="bg-yellow-400 hover:bg-yellow-300 py-2 px-4 rounded w-full"
+          className="bg-yellow-400 hover:bg-yellow-300 py-2 px-4 rounded w-full mt-2"
         >
           Proceed to create an account
         </button>
+
         <hr className="my-4 border-gray-300" />
+
         <b className="text-sm">Already a customer?</b>
         <br />
-        <a href="/signin" className="text-blue-500 text-sm hover:underline">
+        <Link
+          to="/signin"
+          className="text-blue-500 text-sm hover:underline"
+        >
           Sign in with another email or mobile
-        </a>
+        </Link>
       </div>
 
-      <hr className="my-4 border-gray-500" />
+      <hr className="my-4 border-gray-500 w-80" />
+
       <div className="flex justify-center mt-10 gap-4 text-sm text-blue-500">
-        <a href="#" className="hover:underline">
+        <Link to="/conditions" className="hover:underline">
           Conditions of Use
-        </a>
-        <a href="#" className="hover:underline">
+        </Link>
+        <Link to="/privacy" className="hover:underline">
           Privacy Notice
-        </a>
-        <a href="#" className="hover:underline">
+        </Link>
+        <Link to="/help" className="hover:underline">
           Help
-        </a>
+        </Link>
       </div>
 
       <p className="text-xs text-center text-gray-600 mt-4">
